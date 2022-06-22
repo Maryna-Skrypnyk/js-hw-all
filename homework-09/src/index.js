4d304721d162fe5f81bce43dfc0704cb86d3c16b
Maryna Skrypnyk <marina.skrypnyk@gmail.com>
2022-06-22 18:37:38 +0300

    Adds HW-09

---
+++ homework-09/src/index.js
@@ -0,0 +1,142 @@
+import galleryItems from './js/gallery-items.js'; // масив даних
+
+const refs = {
+  galleryList: document.querySelector('ul.js-gallery'),
+  lightbox: document.querySelector('.js-lightbox'),
+  overlay: document.querySelector('.lightbox__overlay'),
+  lightboxImg: document.querySelector('.lightbox__image'),
+  btnClose: document.querySelector(
+    '.lightbox__button[data-action="close-lightbox"]'
+  ),
+}; // посилання для доступу до елементів
+
+/// 1. Створення і рендер розмітки по масиву даних і наданим шаблоном.
+
+const makeGallery = images => {
+  return images
+    .map(({ original, preview, description }) => {
+      return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+    loading="lazy"
+    width="392"
+    height="240"
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+};
+
+refs.galleryList.insertAdjacentHTML('beforeend', makeGallery(galleryItems));
+
+/// 2. Реалізація делегування на галереї ul.js-gallery і
+// отримання url великого зображення
+
+// 3. Відкриття модального вікна при натисканні на елементі галереї.
+
+// 4. Підміна значення атрибута src елемента img.lightbox__image.
+
+const arrGalleryListCopy = [
+  ...refs.galleryList.querySelectorAll('.gallery__image'),
+]; // копія масиву елементів img
+
+const onImgOpen = e => {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  imgModalOpen(e.target.dataset.source, e.target.alt, e.target);
+};
+
+const imgModalOpen = (src, alt, img) => {
+  refs.lightbox.classList.add('is-open');
+  refs.lightboxImg.src = src;
+  refs.lightboxImg.alt = alt;
+  refs.lightboxImg.dataset.index = arrGalleryListCopy.indexOf(img);
+  document.body.classList.add('modal-open'); // при відкритому модальному вікні на body навішуємо клас для відміни скролу
+
+  window.addEventListener('keydown', onModalChangeByKey);
+};
+
+// 5. Закриття модального вікна при натисканні на кнопку
+// button[data - action= ""close - lightbox"].
+
+// 6. Очищення значення атрибута src елемента img.lightbox__image.
+// Це необхідно для того, щоб при наступному відкритті модального вікна,
+// поки вантажиться зображення, ми не бачили попереднє.
+
+// 7. Закриття модального вікна при натисканні на div.lightbox__overlay.
+
+const onModalClose = () => {
+  refs.lightbox.classList.remove('is-open');
+  refs.lightboxImg.src = '';
+  refs.lightboxImg.alt = '';
+  refs.lightboxImg.removeAttribute('data-index');
+  document.body.classList.remove('modal-open'); // при закритті модального вікна з body видаляємо клас, щоб відновити скрол
+
+  window.removeEventListener('keydown', onModalChangeByKey);
+};
+
+// 8. Закриття модального вікна після натискання клавіші ESC.
+
+// 9. Перегортування зображень галереї у відкритому модальному вікні
+// клавішами "вліво" і "вправо".
+
+const onModalChangeByKey = e => {
+  const keys = {
+    ESC: 'Escape',
+    ARROW_RIGHT: 'ArrowRight',
+    ARROW_LEFT: 'ArrowLeft',
+  };
+
+  if (e.code === keys.ESC) {
+    onModalClose();
+  }
+  if (e.code === keys.ARROW_RIGHT) {
+    lightboxImageMove(1);
+  }
+  if (e.code === keys.ARROW_LEFT) {
+    lightboxImageMove(-1);
+  }
+};
+
+const lightboxImageMove = step => {
+  const currentImgIndex = Number(refs.lightboxImg.dataset.index);
+  let nextImgIndex = currentImgIndex + step;
+  if (nextImgIndex < 0) {
+    nextImgIndex = arrGalleryListCopy.length - 1;
+  }
+  if (nextImgIndex > arrGalleryListCopy.length - 1) {
+    nextImgIndex = 0;
+  }
+  refs.lightboxImg.src = arrGalleryListCopy[nextImgIndex].dataset.source;
+  refs.lightboxImg.alt = arrGalleryListCopy[nextImgIndex].alt;
+  refs.lightboxImg.dataset.index = nextImgIndex;
+};
+
+refs.galleryList.addEventListener('click', onImgOpen);
+refs.btnClose.addEventListener('click', onModalClose);
+refs.overlay.addEventListener('click', onModalClose);
+
+/// 10. Реалізація функції lazy-loading при загрузці картинок у браузері.
+
+const imgs = refs.galleryList.querySelectorAll('img[loading="lazy"]');
+
+imgs.forEach(image => {
+  image.addEventListener('load', onImageLoaded, { once: true });
+});
+
+function onImageLoaded(e) {
+  console.log('Картинка загружена');
+  e.target.classList.add('appear');
+}
---
+++ homework-09/src/js/gallery-items.js
@@ -0,0 +1,150 @@
+export default [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_1280.jpg',
+    description: 'Dog',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/02/11/17/00/dog-1194087__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/02/11/17/00/dog-1194087_1280.jpg',
+    description: 'Dog Black',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/02/26/16/32/bulldog-1224267__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/02/26/16/32/bulldog-1224267_1280.jpg',
+    description: 'Bulldog',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/03/27/18/27/sky-1283446__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/03/27/18/27/sky-1283446_1280.jpg',
+    description: 'Sky',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/08/21/08/33/nose-4420535__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/08/21/08/33/nose-4420535_1280.jpg',
+    description: 'Nose',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2015/11/17/13/13/bulldog-1047518__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2015/11/17/13/13/bulldog-1047518_1280.jpg',
+    description: 'French Bulldog Black',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/05/07/10/48/husky-3380548__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/05/07/10/48/husky-3380548_1280.jpg',
+    description: 'Husky',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/09/25/13/12/cocker-spaniel-2785074__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/09/25/13/12/cocker-spaniel-2785074_1280.jpg',
+    description: 'Cocker Spaniel',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/10/26/12/34/fantasy-2890925__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/10/26/12/34/fantasy-2890925_1280.jpg',
+    description: 'Fantasy',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2015/04/23/21/59/tree-736877__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2015/04/23/21/59/tree-736877_1280.jpg',
+    description: 'Tree',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
+    description: 'Cat',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/04/23/19/57/yorkshire-terrier-6202621__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/04/23/19/57/yorkshire-terrier-6202621_1280.jpg',
+    description: 'Yorkshire Terrier',
+  },
+
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
