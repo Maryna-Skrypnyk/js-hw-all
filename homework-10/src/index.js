7f4b2c17eee2344fc9cea13a6da82fd9b4db4b94
Maryna Skrypnyk <marina.skrypnyk@gmail.com>
2022-06-28 14:20:25 +0300

    Adds hw-10

--- homework-10/src/index.js
+++ homework-10/src/index.js
@@ -4,10 +4,45 @@ import './sass/index.scss';
 
 const refs = {
   menuList: document.querySelector('.js-menu'),
+  themeSwitch: document.querySelector('#theme-switch-toggle'),
 };
 
-const menuMarkup = menuTemplate(menu);
+/// 1. Рендер розмітки за шаблоном і даними.
 
-refs.menuList.insertAdjacentHTML('beforeend', menuMarkup);
+refs.menuList.insertAdjacentHTML('beforeend', menuTemplate(menu));
 
-console.log(refs.menuList.insertAdjacentHTML('beforeend', menuMarkup));
+/// 2.
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const THEME_KEY = 'theme';
+
+const replaceThemes = (oldTheme, newTheme) => {
+  document.body.classList.replace(oldTheme, newTheme);
+  localStorage.setItem(THEME_KEY, newTheme);
+};
+
+const onSwitchToggle = e => {
+  if (e.target.checked) {
+    replaceThemes(Theme.LIGHT, Theme.DARK);
+  } else {
+    replaceThemes(Theme.DARK, Theme.LIGHT);
+  }
+};
+
+const savedTheme = () => {
+  const savedTheme = localStorage.getItem(THEME_KEY);
+  if (savedTheme) {
+    document.body.classList.value = savedTheme;
+    refs.themeSwitch.checked = savedTheme === Theme.DARK;
+  } else {
+    document.body.classList.add(Theme.LIGHT);
+  }
+};
+
+savedTheme();
+
+refs.themeSwitch.addEventListener('change', onSwitchToggle);
