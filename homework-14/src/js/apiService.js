c59c237259fa6a450510f37de9726aacca5b0098
Maryna Skrypnyk <marina.skrypnyk@gmail.com>
2022-07-29 17:51:53 +0300

    up

--- homework-14/src/js/apiService.js
+++ homework-14/src/js/apiService.js
@@ -1,8 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
-// const API_KEY = process.env.API_KEY;
-
-const API_KEY = '22687504-cd17fd3731d2b66e1d47a182f';
+const API_KEY = process.env.API_KEY;
 
 const BASE_URL = 'https://pixabay.com/api';
 
